56cf0c8e7554a1c1f5ae35b714dfc3d4d57390dc
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-03-11 22:59:25 +0700

    Docs: Create configuration and base style for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            fontSize: ['hover', 'focus']
+        },
+    },
+    variants: {
+        extend: {
+            outline: ['group-focus']
+        }
+    },
+    plugins: []
+}
