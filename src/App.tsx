18af4c10b8472af9359e6afb44c1062412abc241
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-03-11 23:04:45 +0700

    Docs(Router): Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import './style/main.output.css';
+import browserHistory from "./browserHistory";
+import SignIn from "./auth/page/SignIn";
+import SignUp from "./auth/page/SignUp";
+import ForgotPassword from "./auth/page/ForgotPassword";
+import ResetPassword from "./auth/page/ResetPassword";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router history={browserHistory}>
+        <Switch>
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/forgot" component={ForgotPassword} />
+            <Route path="/reset" component={ResetPassword} />
+        </Switch>
+      </Router>
   );
 }
 
