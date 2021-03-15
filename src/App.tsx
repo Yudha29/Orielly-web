9c5ff9758a94732fcf7a45ba58d1b72f58b0b31e
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-03-15 08:49:00 +0700

    Docs: Add route for Home page and wrap all component with redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Router, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import './App.css';
 import './style/main.output.css';
 import browserHistory from "./browserHistory";
@@ -7,17 +8,22 @@ import SignIn from "./auth/page/SignIn";
 import SignUp from "./auth/page/SignUp";
 import ForgotPassword from "./auth/page/ForgotPassword";
 import ResetPassword from "./auth/page/ResetPassword";
+import Home from "./common/page/home/Home";
+import {store} from "./redux/store";
 
 function App() {
   return (
-      <Router history={browserHistory}>
-        <Switch>
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/forgot" component={ForgotPassword} />
-            <Route path="/reset" component={ResetPassword} />
-        </Switch>
-      </Router>
+        <Provider store={store}>
+            <Router history={browserHistory}>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/signin" component={SignIn} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/forgot" component={ForgotPassword} />
+                    <Route path="/reset" component={ResetPassword} />
+                </Switch>
+            </Router>
+        </Provider>
   );
 }
 
