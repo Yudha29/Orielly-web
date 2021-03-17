8d0e8c83cdda64fc6283894f845a54d0118b177e
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-03-17 15:17:23 +0700

    Refactor: Remove unused import

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,4 @@
-import {createStore, applyMiddleware,} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import {createStore} from 'redux';
 import reducers from "./reducer";
 import {RootState} from "./contract/RootState";
 
