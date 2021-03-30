0a8d89fddecdc0b240b4295219baaa0c64ced4ec
Yudha Nawawi <yudhanawawi12@gmail.com>
2021-03-31 00:50:30 +0700

    Docs: Use lazy load for routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,28 @@ import {Provider} from 'react-redux';
 import './App.css';
 import './style/main.output.css';
 import browserHistory from "./browserHistory";
-import SignIn from "./auth/page/SignIn";
-import SignUp from "./auth/page/SignUp";
-import ForgotPassword from "./auth/page/ForgotPassword";
-import ResetPassword from "./auth/page/ResetPassword";
-import Home from "./common/page/home/Home";
 import {store} from "./redux/store";
-import Search from "./common/page/search/Search";
-import Bag from "./bag/page/Bag";
-import ScrollToTop from "./common/util/ScrollToTop";
-import ProductDetail from "./product/page/productDetail/ProductDetail";
-import PrivacyPolicy from "./common/page/PrivacyPolicy";
-import About from "./common/page/About";
-import Contact from "./common/page/Contact";
-import TermOfUse from "./common/page/TermOfUse";
 
 function App() {
   return (
         <Provider store={store}>
             <Router history={browserHistory}>
-                <ScrollToTop />
+              <React.Suspense fallback={<div>Loading...</div>}>
                 <Switch>
-                    <Route path="/" component={Home} exact />
-                    <Route path="/search" component={Search} />
-                    <Route path="/bag" component={Bag} />
-                    <Route path="/signin" component={SignIn} />
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/forgot" component={ForgotPassword} />
-                    <Route path="/reset" component={ResetPassword} />
-                    <Route path="/contact" component={Contact} />
-                    <Route path="/policies" component={PrivacyPolicy} />
-                    <Route path="/terms" component={TermOfUse} />
-                    <Route path="/about" component={About} />
-                    <Route path="/:id" component={ProductDetail} />
+                  <Route path="/" component={React.lazy(() => import('./common/page/home/Home'))} exact />
+                  <Route path="/search" component={React.lazy(() => import('./common/page/search/Search'))} />
+                  <Route path="/bag" component={React.lazy(() => import('./bag/page/Bag'))} />
+                  <Route path="/signin" component={React.lazy(() => import('./auth/page/SignIn'))} />
+                  <Route path="/signup" component={React.lazy(() => import('./auth/page/SignUp'))} />
+                  <Route path="/forgot" component={React.lazy(() => import('./auth/page/ForgotPassword'))} />
+                  <Route path="/reset" component={React.lazy(() => import('./auth/page/ResetPassword'))} />
+                  <Route path="/contact" component={React.lazy(() => import('./common/page/Contact'))} />
+                  <Route path="/policies" component={React.lazy(() => import('./common/page/PrivacyPolicy'))} />
+                  <Route path="/terms" component={React.lazy(() => import('./common/page/TermOfUse'))} />
+                  <Route path="/about" component={React.lazy(() => import('./common/page/About'))} />
+                  <Route path="/:id" component={React.lazy(() => import('./product/page/productDetail/ProductDetail'))} />
                 </Switch>
+              </React.Suspense>
             </Router>
         </Provider>
   );
